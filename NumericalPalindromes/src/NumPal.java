0255ff9a9bad2e2b48122b17849b02c453d74797
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-10-13 18:59:47 -0700

    feat: Comment on the code and fix a method

--- NumericalPalindromes/src/NumPal.java
+++ NumericalPalindromes/src/NumPal.java
@@ -1,48 +1,92 @@
 
+/**
+ * @author sagesilberman
+ *
+ */
 public class NumPal {
 
-	private long cur;
-	private long rev;
-	private long sum;
-	String curString = "";
-	String revString = "";
+	private long cur; // current number
+	private long rev; // the number reversed
+	private long sum; // the sum of current and reversed number
+	String curString = ""; // current number in String form
+	String revString = ""; // the number reversed in String form
 
+	/**
+	 * Constructs a Palindrome with the start number a.k.a. current number
+	 * 
+	 * @param start the starting number
+	 */
 	public NumPal(String start) {
-		curString = start; // starts as string
-		cur = Long.parseLong(curString); // turns into number
+		curString = start;
+		cur = Long.parseLong(curString);
 
 		for (int i = curString.length(); i > 0; i--) {
-			revString += curString.substring(i - 1, i); // reverses string
+			revString += curString.substring(i - 1, i);
 		}
 
-		rev = Long.parseLong(revString); // turns into string
+		rev = Long.parseLong(revString);
 
 	}
 
-	public long getCur() {
-		return cur;
+	/**
+	 * Returns <code>true</code> if the number is a Palindrome.
+	 *
+	 * ...
+	 * 
+	 * @return<code>true</code> if the number is a Palindrome; <code>false</code> if
+	 * the number isn't the same backwards as it is forwards (a Palindrome)
+	 */
+	public boolean isPalindrome() {
+		int i = 0, j = curString.length() - 1;
+
+		while (i < j) {
+
+			if (curString.charAt(i) != curString.charAt(j))
+				return false;
+
+			i++;
+			j--;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Formats to show that the current number reversed is the reversed number
+	 */
+	public String toString() {
+		return curString + " reverse -> " + revString;
 
 	}
 
-	public long getRev() {
-		return rev;
-
-	}
-
-	public NumPal next() { // adds current and reverse together
+	/**
+	 * Adds the current number and the reverse of that number together
+	 * 
+	 * @return the sum of the current and reverse of that number
+	 */
+	public NumPal next() {
 		sum = cur + rev;
 		return new NumPal(sum + "");
 
 	}
 
-	public boolean isPalindrome() {
-		return false;
+	/**
+	 * Returns the current number
+	 * 
+	 * @return the current number
+	 */
+	public long getCur() {
+		return cur;
 
 	}
 
-	public String toString() { // formats
-		return curString + "reverse ->";
+	/**
+	 * Returns the number in reverse order
+	 * 
+	 * @return the reversed number
+	 */
+	public long getRev() {
+		return rev;
 
 	}
-
 }
